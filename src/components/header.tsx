5c0eaa829998828f0673af8c170b337fea3aabe7
rakshit69 <rakshitagrawal51@gmail.com>
2024-02-09 22:27:02 +0530

    some errors fixed ok

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
+import { signOut } from 'firebase/auth'
+import { useState } from 'react'
+import toast from 'react-hot-toast'
 import { FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
-import { User } from '../types/typesall'
-import { signOut } from 'firebase/auth'
 import { auth } from '../firebase'
-import toast from 'react-hot-toast'
+import { User } from '../types/typesall'
 
 
 interface PropsTypes{
